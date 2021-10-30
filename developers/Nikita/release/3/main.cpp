5f86db28f621742d607d68386c3b98ec5056cb72
Listopadov-Nikita <73636783+Listopadov-Nikita@users.noreply.github.com>
2021-10-30 21:41:51 +0300

    3-й

---
+++ developers/Nikita/release/3/main.cpp
@@ -0,0 +1,56 @@
+#include <fstream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+using namespace std::__cxx11;
+/// ///////////////////////////////////////////////////////////////
+/// пары слов, где на конце 1-го 2-3 гласные а в начале 2-го 2-3 согласные
+void dve_tri_na_koncah(string st)
+{
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a = 0, b = 0, k = 0;
+    string glas = "Єуеыаоэ€ию®”≈џјќЁя»ё";
+    string soglas = "йцкнгшщзхъфвпрлджчсмтьб…÷ Ќ√Ўў«’Џ‘¬ѕ–Ћƒ∆„—ћ“№Ѕ";
+    st += " ";
+    for (int i = 2; i < st.size() - 2; i++)///подсчЄт 1
+        if (st[i] == ' ' && soglas.find(st[i+1]) != string::npos)
+            if (soglas.find(st[i+2]) != string::npos && glas.find(st[i-1]) != string::npos)
+                if (glas.find(st[i-2]) != string::npos)
+                    a++;
+    for (int i = 3; i < st.size() - 3; i++)///подсчЄт 2
+        if (st[i] == ' ' && soglas.find(st[i+1]) != string::npos && soglas.find(st[i+2]) != string::npos)
+            if (soglas.find(st[i+3]) != string::npos && glas.find(st[i-1]) != string::npos)
+                if (glas.find(st[i-2]) != string::npos && glas.find(st[i-3]) != string::npos)
+                    b++;
+
+    for (int i = 0; i < st.size() - 1; i++)///кол-во слов
+        if (st[i] != ' ' && st[i + 1] == ' ')
+            k++;
+    fout << endl;
+    fout << "пар, где по 2 буквы на концах - " << a << endl;
+    fout << "в среднем на пару слов - " << 2 * double(a) / k << endl;
+    fout << "пар, где по 3 буквы на концах - " << b << endl;
+    fout << "в среднем на пару слов - " << 2 * double(b) / k << endl;
+}
+/// ///////////////////////////////////////////////////////////////
+
+int main()
+{
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string st = "", s;
+    while (!fin.eof())
+    {
+        getline(fin, s);
+        s += " ";
+        st += s;
+    }
+    dve_tri_na_koncah(st);
+    return 0;
+}
